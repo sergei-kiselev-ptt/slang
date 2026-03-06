77ffd30d7f722c136d4d6293a794a70f1e3d2efc
Sergei Kiselev <sergey.kiselev.ptt@gmail.com>
2026-03-06 15:18:03 +0100

    comparisons

--- src/qbe.rs
+++ src/qbe.rs
@@ -32,20 +32,23 @@ impl Compiler {
         let mut out = vec![];
         out.push("export function w $main() {".to_string());
         out.push("@start".to_string());
-        let (_tmp, _ty, instructions) = self.compile_expr(&expr).unwrap();
+        let (_tmp, ty, instructions) = self.compile_expr(&expr).unwrap();
         for line in instructions {
             out.push(format!("  {}", line));
         }
-        out.push(format!(
-            "  call $printf(l $fmt, ..., d %t{})",
-            self.counter - 1
-        ));
+        let last = self.counter - 1;
+        let (printf_arg, fmt_spec) = match ty {
+            ResType::Number => (format!("d %t{}", last), "%g"),
+            ResType::Bool => (format!("w %t{}", last), "%d"),
+        };
+        out.push(format!("  call $printf(l $fmt, ..., {})", printf_arg));
         out.push("  ret 0".to_string());
         out.push("}".to_string());
         out.push("\n".to_string());
         out.push(format!(
-            "data $fmt = {{ b \"QBE: {} = %f\\n\", b 0 }}",
-            expr.as_str()
+            "data $fmt = {{ b \"QBE: {} = {}\\n\", b 0 }}",
+            expr.as_str(),
+            fmt_spec
         ));
         out
     }
@@ -126,12 +129,40 @@ impl Compiler {
                         Ok((tmp, ResType::Number, instructions))
                     }
 
-                    TokenType::EqualEqual => todo!(),
-                    TokenType::BangEqual => todo!(),
-                    TokenType::Greater => todo!(),
-                    TokenType::GreaterEqual => todo!(),
-                    TokenType::Less => todo!(),
-                    TokenType::LessEqual => todo!(),
+                    TokenType::EqualEqual => {
+                        let instr = if l_type == ResType::Number {
+                            format!("{} =w ceqd {}, {}", tmp, l_tmp, r_tmp)
+                        } else {
+                            format!("{} =w ceqw {}, {}", tmp, l_tmp, r_tmp)
+                        };
+                        instructions.push(instr);
+                        Ok((tmp, ResType::Bool, instructions))
+                    }
+                    TokenType::BangEqual => {
+                        let instr = if l_type == ResType::Number {
+                            format!("{} =w cned {}, {}", tmp, l_tmp, r_tmp)
+                        } else {
+                            format!("{} =w cnew {}, {}", tmp, l_tmp, r_tmp)
+                        };
+                        instructions.push(instr);
+                        Ok((tmp, ResType::Bool, instructions))
+                    }
+                    TokenType::Greater => {
+                        instructions.push(format!("{} =w cgtd {}, {}", tmp, l_tmp, r_tmp));
+                        Ok((tmp, ResType::Bool, instructions))
+                    }
+                    TokenType::GreaterEqual => {
+                        instructions.push(format!("{} =w cged {}, {}", tmp, l_tmp, r_tmp));
+                        Ok((tmp, ResType::Bool, instructions))
+                    }
+                    TokenType::Less => {
+                        instructions.push(format!("{} =w cltd {}, {}", tmp, l_tmp, r_tmp));
+                        Ok((tmp, ResType::Bool, instructions))
+                    }
+                    TokenType::LessEqual => {
+                        instructions.push(format!("{} =w cled {}, {}", tmp, l_tmp, r_tmp));
+                        Ok((tmp, ResType::Bool, instructions))
+                    }
 
                     TokenType::LogicalOr => todo!(),
                     TokenType::LogicalAnd => todo!(),
@@ -155,4 +186,75 @@ impl Compiler {
 enum QbeError {
     #[error("failed  to compile")]
     CompilationError(String),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::lexer::parse_into_tokens;
+    use crate::parser::Parser;
+
+    fn compile_expr(source: &str) -> (String, ResType, Vec<String>) {
+        let tokens = parse_into_tokens(source).unwrap();
+        let mut parser = Parser::new(tokens);
+        let expr = parser.parse();
+        let mut compiler = Compiler::new();
+        compiler.compile_expr(&expr).unwrap()
+    }
+
+    #[test]
+    fn comparison_equal() {
+        let (tmp, ty, instrs) = compile_expr("1 == 2");
+        assert_eq!(ty, ResType::Bool);
+        assert_eq!(instrs.last().unwrap(), &format!("{} =w ceqd %t0, %t1", tmp));
+    }
+
+    #[test]
+    fn comparison_not_equal() {
+        let (tmp, ty, instrs) = compile_expr("1 != 2");
+        assert_eq!(ty, ResType::Bool);
+        assert_eq!(instrs.last().unwrap(), &format!("{} =w cned %t0, %t1", tmp));
+    }
+
+    #[test]
+    fn comparison_greater() {
+        let (tmp, ty, instrs) = compile_expr("3 > 2");
+        assert_eq!(ty, ResType::Bool);
+        assert_eq!(instrs.last().unwrap(), &format!("{} =w cgtd %t0, %t1", tmp));
+    }
+
+    #[test]
+    fn comparison_greater_equal() {
+        let (tmp, ty, instrs) = compile_expr("3 >= 3");
+        assert_eq!(ty, ResType::Bool);
+        assert_eq!(instrs.last().unwrap(), &format!("{} =w cged %t0, %t1", tmp));
+    }
+
+    #[test]
+    fn comparison_less() {
+        let (tmp, ty, instrs) = compile_expr("1 < 2");
+        assert_eq!(ty, ResType::Bool);
+        assert_eq!(instrs.last().unwrap(), &format!("{} =w cltd %t0, %t1", tmp));
+    }
+
+    #[test]
+    fn comparison_less_equal() {
+        let (tmp, ty, instrs) = compile_expr("2 <= 2");
+        assert_eq!(ty, ResType::Bool);
+        assert_eq!(instrs.last().unwrap(), &format!("{} =w cled %t0, %t1", tmp));
+    }
+
+    #[test]
+    fn bool_equal() {
+        let (tmp, ty, instrs) = compile_expr("true == false");
+        assert_eq!(ty, ResType::Bool);
+        assert_eq!(instrs.last().unwrap(), &format!("{} =w ceqw %t0, %t1", tmp));
+    }
+
+    #[test]
+    fn bool_not_equal() {
+        let (tmp, ty, instrs) = compile_expr("true != false");
+        assert_eq!(ty, ResType::Bool);
+        assert_eq!(instrs.last().unwrap(), &format!("{} =w cnew %t0, %t1", tmp));
+    }
 }
